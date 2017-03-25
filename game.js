aa34a2fd764faa47d1532277b1792faa9ab1706f
Rob <superserva@gmail.com>
2017-03-25 13:12:55 +1300

    add counter

--- game.js
+++ game.js
@@ -3,55 +3,67 @@
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+function start() {
+    bindEventListeners(document.getElementsByClassName('board')[0].children)
 }
 
-function bindEventListeners (dots) {
-  for (var i = 0; i < dots.length; i++) {
-    // BIND YOUR EVENT LISTENERS HERE
-    // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
-    dots[i].addEventListener('click', makeBlue)
-    dots[i].addEventListener('dblclick', hide)
-  }
+function bindEventListeners(dots) {
+    for (var i = 0; i < dots.length; i++) {
+        // BIND YOUR EVENT LISTENERS HERE
+        // The first one is provided for you
+        dots[i].addEventListener('contextmenu', makeGreen)
+        dots[i].addEventListener('click', makeBlue)
+        dots[i].addEventListener('dblclick', hide)
+    }
 }
 
-function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+function makeGreen(evt) {
+    evt.preventDefault()
+    evt.target.classList.toggle('green')
+    updateCounts()
 }
 
 // CREATE FUNCTION makeBlue HERE
 
-function makeBlue (evt) {
-  evt.target.classList.toggle('blue')
-  updateCounts()
+function makeBlue(evt) {
+    evt.target.classList.toggle('blue')
+    updateCounts()
 }
 
 // CREATE FUNCTION hide HERE
 
-function hide (evt) {
-  evt.target.classList.toggle('invisible')
-  updateCounts()
+function hide(evt) {
+    evt.target.classList.toggle('invisible')
+    updateCounts()
 }
 
-function updateCounts () {
-  var totals = {
-    blue: 0,
-    green: 0,
-    invisible: 0
-  }
+function updateCounts() {
+    var totals = {
+        blue: 0,
+        green: 0,
+        invisible: 0
+    }
+    // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+    var dotsArray = document.getElementsByClassName('board')[0].children
 
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+    for (var i = 0; i < dotsArray.length; i++) {
 
-  // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+        if (dotsArray[i].classList.contains('blue')){
+            totals.blue++}
+
+        else if (dotsArray[i].classList.contains('green')) {
+            totals.green++}
+
+        else if (dotsArray[i].classList.contains('invisible')){
+            totals.invisible++}
+    }
+
+    // Once you've done the counting, this function will update the display
+    displayTotals(totals)
 }
 
-function displayTotals (totals) {
-  for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
-  }
+function displayTotals(totals) {
+    for (var key in totals) {
+        document.getElementById(key + '-total').innerHTML = totals[key]
+    }
 }
